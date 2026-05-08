a5a78ba9159a8556fcee35dbfa291ee118cabdc0
Andrey <you@example.com>
2026-05-08 15:18:27 +0300

    доделал до конца hero

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-document.querySelector('.header__search-icon')?.addEventListener('click', () => {
+document.querySelector('.header__search')?.addEventListener('click', () => {
   document.querySelector('.header__search-input')?.focus();
 });
